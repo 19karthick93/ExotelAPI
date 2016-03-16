ac1d5c934c66db3351add1566e832e2c1553f1a9
19karthick93 <karthick@exotel.in>
2016-03-16 21:11:29 +0530

    Update get_balance_api.php

--- PHP_Samples/get_balance_api.php
+++ PHP_Samples/get_balance_api.php
@@ -1,22 +1,24 @@
 <?php
 
-$exotel_sid = "xxxx"; // Your Exotel SID - Get it here: http://my.exotel.in/Exotel/settings/site#exotel-settings
-$exotel_token = "xxxx"; // Your exotel token - Get it here: http://my.exotel.in/Exotel/settings/site#exotel-settings
+function getBalance($exotel_sid, $exotel_token)
+{
+  // To get your Exotel Sid and token, visit https://my.exotel.in/Exotel/settings/site#exotel-settings
  
-$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/balance";
+  $url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/balance";
  
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_VERBOSE, 1);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-$http_result = curl_exec($ch);
-$error = curl_error($ch);
-$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+  $http_result = curl_exec($ch);
+  $error = curl_error($ch);
+  $http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
  
-curl_close($ch);
- 
-print "Response = ".print_r($http_result);
+  curl_close($ch);
+  return $http_result;
+} 
+print_r(getBalance("xxx", "xxx"));
 ?>
